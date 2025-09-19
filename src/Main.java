14e599a1390f866119be9b6c56db6c2e61d7178d
Janssen Gallenero <jansseng3@nycstudents.net>
2025-09-19 12:34:24 -0400

    up to page 6; error with hours

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        DogWalkCompany c = new DogWalkCompany();
+        System.out.println(c);
+        int hour = c.addDogs();
+        System.out.println(c);
+        DogWalker w = new DogWalker(4, c);
+        System.out.println(w.walkDogs(hour) + " dogs walked.");
+        System.out.println(c.numAvailableDogs(hour) + " dogs remaining.");
+    }
+}
